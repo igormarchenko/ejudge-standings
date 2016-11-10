9257daaef4f1ad56847eeff3c2b60ef70673917a
Igor <mar4enko.igor@gmail.com>
2016-11-10 16:48:48 +0200

    Added online updating results.

--- src/main/webapp/app/StandingsController.js
+++ src/main/webapp/app/StandingsController.js
@@ -4,6 +4,7 @@ angular.module("standingsPage", ['ui.select', 'ngSanitize']).controller("standin
 
         var results = {};
         var contestId;
+        var lastSubmitTime = -1;
         $scope.init = function () {
             contestId = window.location.pathname.split('/').pop();
             initResults(contestId);
@@ -58,19 +59,20 @@ angular.module("standingsPage", ['ui.select', 'ngSanitize']).controller("standin
                     $scope.results.push(result);
                 });
 
-                $scope.results.sort(function (a, b) {
-                    if (a.solved == b.solved) {
-                        return a.penalty - b.penalty;
-                    } else {
-                        return b.solved - a.solved;
-                    }
+                $scope.results.sort(compareTeams);
 
-                });
+                console.log($scope.results);
             });
         };
 
         var pushSubmission = function (submission) {
-            var currentTask = results[submission.userId].tasks[submission.problemId];
+            results[submission.userId] = addSubmitToTeam(results[submission.userId], submission);
+            lastSubmitTime = Math.max(lastSubmitTime, submission.time);
+        };
+
+
+        var addSubmitToTeam = function (team, submission) {
+            var currentTask = team.tasks[submission.problemId];
             if (currentTask.status != "OK") {
                 currentTask.submissions.push(submission);
                 currentTask.count++;
@@ -82,10 +84,66 @@ angular.module("standingsPage", ['ui.select', 'ngSanitize']).controller("standin
                 }
                 currentTask.penalty = (currentTask.count - 1) * 20 + currentTask.ok_time;
                 if (submission.status == "OK") {
-                    results[submission.userId].solved++;
-                    results[submission.userId].penalty += currentTask.penalty;
+                    team.solved++;
+                    team.penalty += currentTask.penalty;
                 }
             }
+            return team;
+        };
 
+        var pushSubmitOnline = function (teamPosition, submission) {
+            $scope.results[teamPosition] = addSubmitToTeam($scope.results[teamPosition], submission);
+            lastSubmitTime = Math.max(lastSubmitTime, submission.time);
+        };
+
+        var findTeamPosition = function (teamId) {
+            var result = 0,
+                keepGoing = true;
+            angular.forEach($scope.results, function (team, index) {
+                if (keepGoing && team.contest_team_id == teamId) {
+                    result = index;
+                    keepGoing = false;
+                }
+            });
+            return result;
+        };
+
+        var updateTeamPosition = function (teamPosition) {
+
+            while (true) {
+                if (teamPosition - 1 < 0) break;
+                if (compareTeams($scope.results[teamPosition], $scope.results[teamPosition - 1]) > 0) break;
+
+                var tempTeam = {};
+                angular.copy($scope.results[teamPosition], tempTeam);
+                angular.copy($scope.results[teamPosition - 1], $scope.results[teamPosition]);
+                angular.copy(tempTeam, $scope.results[teamPosition - 1]);
+
+                teamPosition--;
+            }
+        };
+
+        var updateResults = function () {
+            $http({
+                method: 'GET',
+                url: '/api/results/' + contestId + '/' + lastSubmitTime
+            }).success(function (response) {
+                console.log(response);
+                angular.forEach(response, function (submit) {
+                    var team = findTeamPosition(submit.userId);
+                    pushSubmitOnline(team, submit);
+                    updateTeamPosition(team);
+                });
+            })
+        };
+
+        var interval = $interval(updateResults, 1000);
+
+        var compareTeams = function (a, b) {
+            if (a.solved == b.solved) {
+                return a.penalty - b.penalty;
+            } else {
+                return b.solved - a.solved;
+            }
         };
     }]);
